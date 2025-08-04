4c6d8cb80e1660407f31be2ce33b6138d60ee2ca
ALINE ALVES DE SOUZA <25132561@dsn127m1242316.cfp127aluno.local>
2025-08-04 16:56:02 -0300

    add javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const buttonTrocarImagem = document.getElementById('trocar-imagem')
+function trocarImagem(){
+    const cor = document.getElementById.style.setProperty('--bg-img',imagem)
+}
+
+buttonTrocarImagem.addEventListener('click', trocarImagem)
